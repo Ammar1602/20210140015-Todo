6bb347c0dd2e90645697bdc3ec1503fab9169264
Ammar Fatwa Syuhada <115075702+Ammar1602@users.noreply.github.com>
2024-04-05 21:46:10 +0800

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,37 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\clear;
 use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
-        dd($todos);
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+        //dd($todos);
+        return view('todo.index', compact('todos'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('todo.create');
     }
+    public function store(Request $request, Todo $todo)
+    {
+        //
+        $request->validate([
+            'title' => 'required|max:255'
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+        $todo = Todo::created([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+    }
     public function edit()
     {
         return view('todo.edit');
     }
-
-
 }
