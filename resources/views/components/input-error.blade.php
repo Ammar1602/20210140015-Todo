a75325cb07318c601321024634a3279d54bc9dea
Ammar Fatwa Syuhada <115075702+Ammar1602@users.noreply.github.com>
2024-04-30 17:09:10 +0700

    “middleware, gate admin”

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,8 +1,10 @@
-@props(['message'])
+@props(['messages'])
 
-@if ($message)
+
+
+@if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
-        @foreach ((array) $messages as $messag)
+        @foreach ((array) $messages as $messages)
             <li>{{ $messages }}</li>
         @endforeach
     </ul>
